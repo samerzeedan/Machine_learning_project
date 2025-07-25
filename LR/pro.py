a1f0630eac1485ed8d2a343fe7ace2820ce805b2
samerzeedan <aymanzeedan09@gmail.com>
2025-07-25 15:45:32 +0300

    my smsm

---
+++ LR/pro.py
@@ -0,0 +1,8 @@
+عشان المشاريع ونجيب ال gethub and redme and clone
+cd /
+cd Users
+mkdir My-GitHub
+cd My-GitHub
+git clone https://github.com/samerzeedan/Machine_learning_project.git
+dir
+cd Machine_learning_project
